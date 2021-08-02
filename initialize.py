f04a1292979169b9d5b2c99e6b30c88fffca73dc
Cameron <charliesinfiniteride@gmail.com>
2021-08-02 11:14:55 -0400

    Stubs

---
+++ initialize.py
@@ -0,0 +1,26 @@
+# interactive setup
+# pull in
+#	sites
+#	recipients
+
+# Maybe in the future guide through the Gmail setup part
+
+
+
+# check for config.json, and read if available, else start a new one
+
+# Sites to Check
+# Display a list of current sites if available, and ask to add sites
+# one per line until blank
+
+# Persons to notify
+# Display a list of current recipients if available, and ask to add
+# one per line until blank
+
+# Cron setup
+# Display current Cron state, and ask to modify
+# Checking daily as default
+
+# Maybe build for configurable-per-site later
+
+
